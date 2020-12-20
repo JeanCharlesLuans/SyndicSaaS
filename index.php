d7938f80311c030ca635d4580e3978bbc5237d3b
Epoxydel <47179646+Epoxydel@users.noreply.github.com>
2020-12-20 17:46:38 +0100

    Initialisation projet

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title> Test </title>
+		<link href="bootstrap/CSS/bootstrap.css" rel="stylesheet">
+		<link href="fontawesome/css/all.css" rel="stylesheet">
+	</head>
+	<body>
+	</body>
+	<script type="text/javascript" src="jquery/jquery.js"></script>
+</html>
