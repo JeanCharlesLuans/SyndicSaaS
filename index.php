64374125d28d492b49285ab72805926314ebaacf
Epoxydel <47179646+Epoxydel@users.noreply.github.com>
2021-01-02 16:14:32 +0100

    Ajout partie authentfication

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
 		<title> Test </title>
 		<link href="bootstrap/CSS/bootstrap.css" rel="stylesheet">
 		<link href="fontawesome/css/all.css" rel="stylesheet">
+		<link href="css/authentification.css" rel="stylesheet">
 	</head>
 	<body>
+
 	</body>
 	<script type="text/javascript" src="jquery/jquery.js"></script>
+	<script type="text/javascript" src="jquery/authentification.js"></script>
 </html>
