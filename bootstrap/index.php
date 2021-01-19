95ea867f7ef624522c42bf3802fd6856299cb818
Epoxydel <47179646+Epoxydel@users.noreply.github.com>
2021-01-19 21:39:01 +0100

    PageInterdictionAcces
    Interdit l'acces au dossier dont l'utilisateur ne doit pas accéder

---
+++ bootstrap/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title> SyndicSaaS Interdiction </title>
+		<link href="../bootstrap/CSS/bootstrap.css" rel="stylesheet">
+		<link href="../css/pageInterdiction.css" rel="stylesheet">
+	</head>
+	<body>
+        <div class="container">
+            <div class="row margeTop">
+				<div class="col-12 centrer">
+                	<h1>ERROR 403</h1>
+				</div>
+				<div class="col-12 centrer">
+					<h2>Forbidden</h2><br/>
+				</div>
+				<div class="col-12 centrer">
+					<h3>Vous n'avez pas la permissions d'accéder à cette ressource<br/><br/>Cliquer sur l'image pour retourner au menu :</h3>
+				</div>
+				<div class="col-12 centrer">
+					<a href="../index.php"><img src="../img/Logo_SyndicSaaS.png" class="tailleImg"></a>
+				</div>
+            </div>
+        </div>
+	</body>
+	<script type="text/javascript" src="jquery/jquery.js"></script>
+	<script type="text/javascript" src="jquery/authentification.js"></script>
+</html>
--- page/index.php
+++ page/index.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
-		<title> SyndicSaaS Authentification </title>
+		<title> SyndicSaaS Interdiction </title>
 		<link href="../bootstrap/CSS/bootstrap.css" rel="stylesheet">
-		<link href="../fontawesome/css/all.css" rel="stylesheet">
-		<link href="../css/pageInterdiction" rel="stylesheet">
+		<link href="../css/pageInterdiction.css" rel="stylesheet">
 	</head>
 	<body>
         <div class="container">
-            <div class="row">
-                <h1>Vous n'avez pas les permissions pour accéder à cette partie du site !</h1>
-                <h2>Vous pouvez retourner à l'accueil</h2>
+            <div class="row margeTop">
+				<div class="col-12 centrer">
+                	<h1>ERROR 403</h1>
+				</div>
+				<div class="col-12 centrer">
+					<h2>Forbidden</h2><br/>
+				</div>
+				<div class="col-12 centrer">
+					<h3>Vous n'avez pas la permissions d'accéder à cette ressource<br/><br/>Cliquer sur l'image pour retourner au menu :</h3>
+				</div>
+				<div class="col-12 centrer">
+					<a href="../index.php"><img src="../img/Logo_SyndicSaaS.png" class="tailleImg"></a>
+				</div>
             </div>
         </div>
 	</body>
