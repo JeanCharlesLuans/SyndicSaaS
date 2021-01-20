3386d6ec89d83561979823ad0e55e1a1945f0287
Epoxydel <47179646+Epoxydel@users.noreply.github.com>
2021-01-20 23:29:11 +0100

    Push modification admin

--- page/contenuAdmin.php
+++ page/contenuAdmin.php
@@ -5,19 +5,47 @@
 		<link href="../fontawesome/css/all.css" rel="stylesheet">
 		<link href="../css/contenuAdmin.css" rel="stylesheet">
 	</head>
-	<body>
+	<body class="background">
 		<header class="headerPcTab">
 				<img src='../img/logo_sans_fond.png' class="imgPcTab">
 				<p id="iconeCopro" class="navAdmin copro"><i class="fas fa-house-user"></i> Copropriété </p>
 				<p id="iconeUser" class="navAdmin user"><i class="fas fa-users"></i> Utilisateur </p>
 				<a class="deconnexion" href="../index.php"> <i class="fas fa-power-off"></i> </a>
 		</header>
-		<div class="container">
-			<div id="divCopropriete">
+		<div class="container backgroundContainer">
+			<div id="divCoproprietes">
 				<h1 class="titre">Les copropriétés sur l'application</h1>
+				<p class="ajoutCopro"><i class="fas fa-plus"></i> Copropriété</p>
+				<div class="divCopropriete row">
+					<div class="col-12">
+						<h2 class="titreNom"> Sainte herves de la Rue pierre <h2>
+					</div>
+					<div class="col-6">
+						<i class="fas fa-trash fa-2x"></i>
+					</div>
+					<div class="col-6">
+						<i class="far fa-edit fa-2x"></i>
+					</div>
+					</div class="col-12">
+						<h3 class="titreRubrique"> Gestionnaire de copropriété :</h3>
+					</div>
+					</div class="col-12">
+						<h3 class="titreRubrique"> Copropriétaire :</h3>
+					</div>
+				</div>
 			</div>
-			<div id="divUser" class="d-none">
+			<div id="divUsers" class="d-none">
 				<h1 class="titre">Utilisateur de l'application</h1>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
 			</div>
 		</div>
     </body>
