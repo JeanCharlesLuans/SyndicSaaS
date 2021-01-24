d4ff95d691d65b2b2748734149bf16d8d7cdeb3b
Epoxydel <47179646+Epoxydel@users.noreply.github.com>
2021-01-24 22:02:44 +0100

    gestionCO

--- page/gestionCopropriete.php
+++ page/gestionCopropriete.php
@@ -1,15 +1,58 @@
 <?php  $pdo = new PDO('mysql:host=localhost;dbname=syndicsaas', 'root', '');  ?>
 
-<!DOCTYPE html>
 <html>
-<head>
-	<title>Gestion Copropriété</title>
-	<link href="bootstrap/CSS/bootstrap.css" rel="stylesheet">
-	<link href="fontawesome/css/all.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="/css/style.css"/>
-</head>
-<body>
-	<h1>Gestion des copropriétés</h1>
-	
-</body>
+    <head>
+		<title> SyndicSaaS Gestionnaire Copropriété </title>
+		<link href="../bootstrap/CSS/bootstrap.css" rel="stylesheet">
+		<link href="../fontawesome/css/all.css" rel="stylesheet">
+		<link href="../css/contenuPageGestion.css" rel="stylesheet">
+	</head>
+	<body class="background">
+		<header class="headerPcTab d-none d-sm-block">
+				<img src='../img/logo_sans_fond.png' class="imgPcTab">
+				<a class="deconnexion" href="../index.php"> <i class="fas fa-power-off"></i> </a>
+		</header>
+		<header class="headerMobil d-block d-sm-none">
+				<img src='../img/logo_sans_fond.png' class="imgMobil">
+				<a class="deconnexionMobil" href="../index.php"> <i class="fas fa-power-off"></i> </a>
+		</header>
+		<div class="container backgroundContainer">
+			<div id="divCoproprietes">
+				<h1 class="divCenter">Vos copropriétés</h1>
+				<div id="divCopro1" class="divCopropriete row">
+					<div class="col-12">
+						<h2 class="titreNom"> Sainte herves de la Rue pierre </h2>
+					</div>
+					<br/><br/>
+					<div class="col-12 divCenter">
+						<p class="ajout"><i class="fas fa-users"></i> Convocation assemblé</p>
+					</div>
+					<div class="col-12 divCenter">
+						<h3 class="titreRubrique"> Gestionnaire :</h3>
+						<div></div>
+						<div class="row">
+							<div class="col-sm-2 d-none d-sm-block"> ID</div>
+							<div class="col-sm-4 d-none d-sm-block"> NOM</div>
+							<div class="col-sm-4 d-none d-sm-block"> PRENOM</div>
+							<div class="col-sm-2"></div>
+						</div>
+						<div class="row">
+							<div class="col-12 col-sm-2"><input type="number" value="1" disabled class="inputUser"></div>
+							<div class="col-12 col-sm-4"><input type="text" value="Dumas" disabled class="inputUser"></div>
+							<div class="col-12 col-sm-4"><input type="text" value="Michel" disabled class="inputUser"></div>
+							<div class="col-12 col-sm-2"><input type="number" class="inputUser" placeholder="millieme"></div>
+						</div>
+					</div>
+					
+					
+				</div>
+			</div>
+		<div>
+		<footer class="footer">
+			<p>Mention légal</p>
+		</footer>
+	</body>
+	<div class="modificationUser">
+	</div>
+	<script type="text/javascript" src="../jquery/jquery.js"></script>
 </html>
