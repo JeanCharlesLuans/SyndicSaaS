6d0a90175b3e408bae2d4fff71b6d4759a179e83
Epoxydel <47179646+Epoxydel@users.noreply.github.com>
2021-01-27 21:15:01 +0100

    Update addCopro.php

--- php/ajax/addCopro.php
+++ php/ajax/addCopro.php
@@ -2,12 +2,6 @@
 /* Dossiers ajoutCopro */
 $pdo = new PDO('mysql:host=localhost;dbname=syndicsaas', 'root', '');
 
-echo '<br/>nom : '.$_GET['nom'];
-echo '<br/>adresse : '.$_GET['adresse'];
-echo '<br/>ville : '.$_GET['ville'];
-echo '<br/>codePostal'.$_GET['codePostal'];
-
-
-$sql = 'INSERT INTO copropriete (ID, NOM, ADRESSE, VILLE,CODE_POSTAL) VALUES (?,?,?,?,?)';
-$pdo->prepare($sql)->execute([$_GET['id'],$_GET['nom'], $_GET['adresse'], $_GET['ville'], $_GET['codePostal']]);
+$sql = 'INSERT INTO copropriete (NOM, ADRESSE, VILLE,CODE_POSTAL) VALUES (?,?,?,?,?)';
+$pdo->prepare($sql)->execute([$_GET['nom'], $_GET['adresse'], $_GET['ville'], $_GET['codePostal']]);
 ?>
